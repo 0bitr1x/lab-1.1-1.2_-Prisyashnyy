40bf38d002ff9c7020194c166d4f676752ed2f12
0bitr1x <144239816+0bitr1x@users.noreply.github.com>
2023-09-27 20:41:25 +0400

    Update Program.cs

--- lab№1_ Prisyashnyy/Program.cs
+++ lab№1_ Prisyashnyy/Program.cs
@@ -1,5 +1,4 @@
-﻿//VAR 12
-//Высокий уровень/задание 1.1
+﻿//учебник "сборник задач по программированию"/тема: Арифметические выражения/высокий уровень/вариант_12
 Console.Write("Введите y: ");
 double y = double.Parse(Console.ReadLine());
 Console.Write("Введите x: ");
@@ -7,17 +6,11 @@ double x = double.Parse(Console.ReadLine());
 Console.Write("Введите a: ");
 double a = double.Parse(Console.ReadLine());
 double G = ((Math.Pow(Math.Cos(Math.Abs(y + x)), 3) - (x + y))) / (Math.Pow(Math.Atan(x + a), 4) * Math.Pow(x, 5));
-Console.WriteLine($"G = {G}");
-//Высокий уроыень/задание 1.2
+Console.WriteLine($"G = {G:F2");
+//учебник "сборник задач по программированию"/тема: Программирование линейных алгоритмов/высокий уровень/вариант_12
 Console.Write("Введите a: ");
 double a = double.Parse(Console.ReadLine());
 Console.Write("Введите b: ");
 double b = double.Parse(Console.ReadLine());
-if(a >= b)
-{
     double x = Math.Floor(a / b);
     Console.WriteLine($"На отрезке a помешается отрезков b: {x}");
-} else
-{
-    Console.WriteLine("отрезок b меньше отрезка a");
-}
