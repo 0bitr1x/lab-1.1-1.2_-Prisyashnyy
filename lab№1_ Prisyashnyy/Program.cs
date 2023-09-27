1b1a954b9c6d57a3062fdac7170b8ccb3f4ef298
0bitr1x <144239816+0bitr1x@users.noreply.github.com>
2023-09-27 21:25:26 +0400

    Update Program.cs

--- lab№1_ Prisyashnyy/Program.cs
+++ lab№1_ Prisyashnyy/Program.cs
@@ -6,11 +6,11 @@ double x = double.Parse(Console.ReadLine());
 Console.Write("Введите a: ");
 double a = double.Parse(Console.ReadLine());
 double G = ((Math.Pow(Math.Cos(Math.Abs(y + x)), 3) - (x + y))) / (Math.Pow(Math.Atan(x + a), 4) * Math.Pow(x, 5));
-Console.WriteLine($"G = {G:F2");
+Console.WriteLine($"G = {G:F2}");
 //учебник "сборник задач по программированию"/тема: Программирование линейных алгоритмов/высокий уровень/вариант_12
 Console.Write("Введите a: ");
 double a = double.Parse(Console.ReadLine());
 Console.Write("Введите b: ");
 double b = double.Parse(Console.ReadLine());
-    double x = Math.Floor(a / b);
-    Console.WriteLine($"На отрезке a помешается отрезков b: {x}");
+double x = Math.Floor(a / b);
+Console.WriteLine($"На отрезке a помешается отрезков b: {x}");
